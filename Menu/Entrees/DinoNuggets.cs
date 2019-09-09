0e71472cbab5a5138c0e3f2d9c1064697dd59423
Anthony Garvalena <ajgarval@users.campus>
2019-09-09 17:20:39 -0500

    Brontowurst, DinoNuggets, PterodactylWings Completed

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class DinoNuggets
+    {
+        private int nuggetCount = 0;
+
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "Chicken Nugget" };
+                return ingredients;
+            }
+        }
+
+        public DinoNuggets()
+        {
+            this.Price = 4.25;
+            this.Calories = 498;
+        }
+
+        public void AddNugget()
+        {
+            nuggetCount++;
+            Price += .25;
+            Calories += 59;
+        }
+
+
+    }
+}
